a8d989461187e6220a606ad8a2efef9782778381
dangdinhsi <siddd00474@fpt.edu.vn>
2018-11-24 10:04:49 +0700

    Exam dot net core MVC 24/11/2018

---
+++ MVCdotNetCoreT1708M1/Data/MVCdotNetCoreT1708M1Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCdotNetCoreT1708M1.Models;
+
+namespace MVCdotNetCoreT1708M1.Models
+{
+    public class MVCdotNetCoreT1708M1Context : DbContext
+    {
+        public MVCdotNetCoreT1708M1Context (DbContextOptions<MVCdotNetCoreT1708M1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCdotNetCoreT1708M1.Models.Category> Category { get; set; }
+
+        public DbSet<MVCdotNetCoreT1708M1.Models.Product> Product { get; set; }
+    }
+}
